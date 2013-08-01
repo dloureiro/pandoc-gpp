b5ce8773df8e985c9cb105d4b8c378688da21ba0
David Loureiro <david.loureiro@sysfera.com>
2013-08-01 19:11:56 +0200

    Adding setup.py script for installation of pandoc-gpp

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(name='pandoc-gpp',
+      version='1.0',
+      scripts=['bin/pandoc-gpp'],
+      data_files=[('include', ['include/macros.gpp', 'include/html.gpp', 'include/latex.gpp']),
+      ('share/bin', ['share/bin/evaluateExpression.py','share/bin/generate_tabular.py', 'share/bin/generate_graph.py', 'share/bin/includeFile.py']),
+      ('share/doc/pandoc-gpp-readme.md', ['README.md'])],
+      description='pandoc-gpp is wrapper around pandoc and gpp to provide mode functionalities to pandoc users',
+      author='David Loureiro',
+      author_email='david.loureiro1@gmail.com',
+      url='https://github.com/dloureiro/pandoc-gpp',
+      )
