cf7c0ea43aaea77f7c9a4e81b81795b755fa5218
David Loureiro <david.loureiro@sysfera.com>
2013-08-02 09:40:16 +0200

    Adding dependencies (pygal, cairosvg and cssselect

--- setup.py
+++ setup.py
@@ -9,4 +9,5 @@ setup(name='pandoc-gpp',
       author='David Loureiro',
       author_email='david.loureiro1@gmail.com',
       url='https://github.com/dloureiro/pandoc-gpp',
+      requires=["pygal (>=1.0.0)", "CairoSVG (>=0.5)", "cssselect (>0.8)"]
       )
