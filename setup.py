8f24b998e9cf1f540b9405e911d241e41cdb15a4
David Loureiro <david.loureiro@sysfera.com>
2013-08-03 15:21:13 +0200

    Changing version to 0.2.1 and indenting data_files

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
 
 setup(name='pandoc-gpp',
-      version='0.2',
+      version='0.2.1',
       scripts=['bin/pandoc-gpp'],
-      data_files=[('include', ['include/macros.gpp', 'include/html.gpp', 'include/latex.gpp']),
-      ('share/bin', ['share/bin/evaluateExpression.py','share/bin/generate_tabular.py', 'share/bin/generate_graph.py', 'share/bin/includeFile.py']),
-      ('share/doc/pandoc-gpp-readme.md', ['README.md'])],
+      data_files=[
+            ('include', ['include/macros.gpp', 'include/html.gpp', 'include/latex.gpp']),
+            ('share/bin', ['share/bin/evaluateExpression.py','share/bin/generate_tabular.py', 'share/bin/generate_graph.py', 'share/bin/includeFile.py']),
+            ('share/doc/pandoc-gpp-readme.md', ['README.md'])
+            ],
       description='pandoc-gpp is wrapper around pandoc and gpp to provide mode functionalities to pandoc users',
       author='David Loureiro',
       author_email='david.loureiro1@gmail.com',
