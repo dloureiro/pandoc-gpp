b5382412eec07045ddf468d3094a5ac80032f39c
David Loureiro <david.loureiro@sysfera.com>
2013-08-05 23:13:58 +0200

    chg: dev: changing README file name and updating version

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='pandoc-gpp',
-      version='0.2.4',
+      version='0.2.5',
       scripts=['bin/pandoc-gpp'],
       data_files=[
             ('include', ['include/macros.gpp', 'include/html.gpp', 'include/latex.gpp']),
             ('share/bin', ['share/bin/evaluateExpression.py','share/bin/generate_tabular.py', 'share/bin/generate_graph.py', 'share/bin/includeFile.py']),
-            ('share/doc/pandoc-gpp-readme', ['README'])
+            ('share/doc/pandoc-gpp-readme.md', ['README.md'])
             ],
       description='pandoc-gpp is wrapper around pandoc and gpp to provide mode functionalities to pandoc users',
       author='David Loureiro',
